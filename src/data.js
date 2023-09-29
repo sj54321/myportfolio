aece32e03e80ad502c4ba737c613a98363c601cc
jaspreet <rs9122000@gmail.com>
2023-09-29 11:48:48 +0530

    added my portfolio project

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "Youtube clone",
+    subtitle: "React based app",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.png",
+    link: "https://cozy-manatee-1f49d7.netlify.app/",
+  }
+ 
+];
+
+export const experiances = [
+  {
+    quote:
+      "Has Contributed to an Express Based E-commerce API on GitHub can be used for E-commerce sites",
+      image: "https://yt3.googleusercontent.com/ytc/AGIKgqMNyJ6v7zs1fuFQUjCjJ3rTGM4XbAYQN5eBhOZ-VA=s900-c-k-c0x00ffffff-no-rj",
+    name: "Front-End Engineer",
+    company: "Coding Blocks",
+  },
+  // {
+  //   quote:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+  //   image: "https://w7.pngwing.com/pngs/36/959/png-transparent-meta-logo-facebook-social-media-chat-message-communication-icon-thumbnail.png",
+  //   name: "React-Developer",
+  //   company: "Meta",
+  // },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Bootstrap",
+  "Node",
+  "Tailwind CSS",
+  "Material UI",
+];
